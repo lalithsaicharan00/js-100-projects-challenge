0bc54781a07ce89b931bea28e24a60aadbf583c9
lalith-sai-charan-9 <lalithsaicharan9@gmail.com>
2025-05-03 13:24:03 +0530

    challenge 7 completed

---
+++ 07 string lenght and case/script.js
@@ -0,0 +1,9 @@
+let text = prompt("Enter some text");
+
+let textLenght = text.length;
+let textUppercase = text.toUpperCase();
+let textLowercase = text.toLowerCase();
+
+console.log("length :" + textLenght);
+console.log("upppercase : " + textUppercase);
+console.log("lowercase : " + textLowercase);
