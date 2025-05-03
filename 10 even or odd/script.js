9e54f365c48e35774fd97df78dc5af539cccb173
lalith-sai-charan-9 <lalithsaicharan9@gmail.com>
2025-05-03 13:55:35 +0530

    challenge 10 completed

---
+++ 10 even or odd/script.js
@@ -0,0 +1,8 @@
+let num = prompt("Enter a whole number");
+num = Number(num);
+
+if (num % 2 == 0) {
+  console.log(`the number ${num} is even number`);
+} else {
+  console.log(`the number ${num} is odd number`);
+}
