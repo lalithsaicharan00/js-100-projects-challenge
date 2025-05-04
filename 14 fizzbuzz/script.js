eda0c74d86fcb8c870e019225cefb42e43ecf9ce
lalith-sai-charan-9 <lalithsaicharan9@gmail.com>
2025-05-04 08:57:48 +0530

    challenge 14 completed

---
+++ 14 fizzbuzz/script.js
@@ -0,0 +1,13 @@
+let number = Number(prompt("Enter a number:"));
+
+for (let i = 1; i <= number; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
