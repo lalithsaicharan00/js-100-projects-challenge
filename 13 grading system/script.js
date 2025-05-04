d5bea91bb006014c47ea030dfedd587107e23d77
lalith-sai-charan-9 <lalithsaicharan9@gmail.com>
2025-05-04 08:52:47 +0530

    challenge 13 completed

---
+++ 13 grading system/script.js
@@ -0,0 +1,16 @@
+let score = Number(prompt("Enter your score:"));
+let grade = "";
+
+if (score >= 90) {
+  grade = "A";
+} else if (score >= 80) {
+  grade = "B";
+} else if (score >= 70) {
+  grade = "C";
+} else if (score >= 60) {
+  grade = "D";
+} else {
+  grade = "F";
+}
+
+alert("Your grade is: " + grade);
